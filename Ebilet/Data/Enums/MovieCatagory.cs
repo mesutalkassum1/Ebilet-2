9642d99ea6867ff68aa3d333f4fd62f97755950d
mesutalkassum1 <masood.alkassom@ogr.sakarya.edu.tr>
2022-12-16 12:08:04 +0300

    Add project files.

---
+++ Ebilet/Data/Enums/MovieCatagory.cs
@@ -0,0 +1,12 @@
+﻿namespace Ebilet.Data.Enums
+{
+    public enum MovieCatagory
+    {
+        Action = 1,
+        Comedy,
+        Drama,
+        Documentary,
+        Horror,
+        Cartoon
+    }
+}
---
+++ Ebilet/Models/Actor.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Ebilet.Models
+{
+    public class Actor
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string FullName { get; set; }
+        public string Bio { get; set; }
+        //Relationships
+        public List<Actor_Movie> Actors_Movies { get; set; }
+    }
+}
