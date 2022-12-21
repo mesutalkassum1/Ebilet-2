1668fdc520416eccc2788547c2632c904350a895
mesutalkassum1 <masood.alkassom@ogr.sakarya.edu.tr>
2022-12-21 12:49:30 +0300

    .

---
+++ Ebilet/Controllers/CinemasController.cs
@@ -0,0 +1,19 @@
+﻿using Ebilet.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ebilet.Controllers
+{
+    public class CinemasController : Controller
+    {
+        private readonly AppDbContext _context;
+        public CinemasController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var allCinemas = _context.Cinemas.ToList();
+            return View();
+        }
+    }
+}
--- Ebilet/Models/Actor.cs
+++ Ebilet/Models/Actor.cs
@@ -6,10 +6,15 @@ namespace Ebilet.Models
     {
         [Key]
         public int Id { get; set; }
+
         [Required]
-        public string FullName { get; set; }
-        public string Bio { get; set; }
+        [Display(Name = "Profile Picture")]
+        public string? ProfilePictureURL { get; set; }
+        [Display(Name = "FullName")]
+        public string? FullName { get; set; }
+        [Display(Name = "Bioghraphy")]
+        public string? Bio { get; set; }
         //Relationships
-        public List<Actor_Movie> Actors_Movies { get; set; }
+        public List<Actor_Movie>? Actors_Movies { get; set; }
     }
 }
