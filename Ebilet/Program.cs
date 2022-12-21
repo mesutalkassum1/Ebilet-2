4722619ad28a656b0a78687ed4a7f398fc2696f7
mesutalkassum1 <masood.alkassom@ogr.sakarya.edu.tr>
2022-12-21 18:34:45 +0300

    .

--- Ebilet/Program.cs
+++ Ebilet/Program.cs
@@ -1,14 +1,20 @@
 using Ebilet.Data;
 using Microsoft.EntityFrameworkCore;
+using Ebilet.Data.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
+//Service cofiguration
+builder.Services.AddScoped<IActorsService, ActorsService>();
+
 // Add services to the container.
+
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<AppDbContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
