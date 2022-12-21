6c4a89262ac39549838a2a5345191c12a0748dbe
mesutalkassum1 <masood.alkassom@ogr.sakarya.edu.tr>
2022-12-21 13:39:57 +0300

    .

--- Ebilet/Controllers/MoviesController.cs
+++ Ebilet/Controllers/MoviesController.cs
@@ -13,7 +13,7 @@ namespace Ebilet.Controllers
         }
         public async Task<IActionResult> Index()
         {
-            var allMovies = await _context.Movies.Include(n => n.Cinema).ToListAsync();
+            var allMovies = await _context.Movies.Include(n => n.Cinema).OrderBy(n => n.Name).ToListAsync();
             return View(allMovies);
         }
     }
